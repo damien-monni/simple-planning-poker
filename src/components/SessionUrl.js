f2a795cbe7be638fb5a910e1a3cbb146cdc48407
Damien Monni <contact@damien-monni.com>
2019-03-17 19:21:06 -0300

    Improve design

--- src/components/SessionUrl.js
+++ src/components/SessionUrl.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   root: {
@@ -12,6 +10,9 @@ const useStyles = makeStyles({
     borderTopWidth: 0,
     borderRadius: '0 0 20px 20px',
     boxShadow: '0 0 30px rgba(33, 150, 243, 0.28)',
+  },
+  url: {
+    display: 'initial',
   },
 });
 
@@ -24,9 +25,9 @@ export default () => {
     <div className={classes.root}>
       <Typography>
         <b>URL de partage : </b>
-        <Link component={RouterLink} to={url}>
+        <Typography className={classes.url} component="span" color="primary">
           {url}
-        </Link>
+        </Typography>
       </Typography>
     </div>
   );
