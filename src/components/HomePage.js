0294a4edd8dd72634218eaf060e95cad450ee8f3
Damien Monni <contact@damien-monni.com>
2019-03-18 16:39:39 -0300

    Design home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Typography from '@material-ui/core/Typography';
 
 import useStartSession from '../hooks/useStartSession';
 import StartSession from './StartSession';
 
+const useStyles = makeStyles((theme) => ({
+  main: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginBottom: 80,
+  },
+}));
+
 export default (props) => {
   const { history } = props;
+
+  const classes = useStyles();
 
   const { name, onNameChange, onSubmit } = useStartSession({ history });
 
   return (
-    <StartSession
-      nameInputValue={name}
-      onNameInputChange={onNameChange}
-      onSubmit={onSubmit}
-    />
+    <main className={classes.main}>
+      <Typography className={classes.title} variant="h1" color="inherit">
+        Simple Planning Poker
+      </Typography>
+      <StartSession
+        nameInputValue={name}
+        onNameInputChange={onNameChange}
+        onSubmit={onSubmit}
+      />
+    </main>
   );
 };
--- src/components/StartSession.js
+++ src/components/StartSession.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    backgroundColor: 'white',
+    boxShadow: '0 0 30px rgba(0, 0, 0, 0.25)',
+    padding: 50,
+    borderRadius: 10,
+    width: 300,
+  },
+  textField: {
+    marginBottom: 20,
+  },
+}));
 
 export default (props) => {
   const { nameInputValue, onNameInputChange, onSubmit } = props;
 
+  const classes = useStyles();
+
   return (
-    <form onSubmit={onSubmit}>
-      <TextField
-        label="Nom"
-        value={nameInputValue}
-        onChange={onNameInputChange}
-      />
-      <Button type="submit" variant="contained" color="primary">
+    <form className={classes.form} onSubmit={onSubmit}>
+      <div className={classes.textField}>
+        <TextField
+          label="Votre nom"
+          value={nameInputValue}
+          fullWidth
+          variant="outlined"
+          autoFocus
+          onChange={onNameInputChange}
+        />
+      </div>
+      <Button
+        fullWidth
+        type="submit"
+        variant="contained"
+        color="secondary"
+        disabled={!nameInputValue.length}
+      >
         Démarrer une nouvelle session
       </Button>
     </form>
